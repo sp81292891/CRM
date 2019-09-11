8f87af423a4a95466452285bf5bbbc6bc3d50ef7
sp81292891 <spsprasad46@gmial.com>
2019-09-11 16:06:31 +0530

    sales report

--- src/app/salesreport/salesreport.module.ts
+++ src/app/salesreport/salesreport.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SalesreportComponent } from './salesreport/salesreport.component';
 import {SalesreportRoutingModule} from './salesreport-routing.module'
+import{FormsModule} from'@angular/forms'
 
+import {InputTextModule} from 'primeng/inputtext';
+import {DropdownModule} from 'primeng/dropdown';
+import {CalendarModule} from 'primeng/calendar';
+import {RadioButtonModule} from 'primeng/radiobutton';
+import {CheckboxModule} from 'primeng/checkbox';
+import {ButtonModule} from 'primeng/button';
+import {TableModule} from 'primeng/table';
+
+import {DialogModule} from 'primeng/dialog';
 
 @NgModule({
   declarations: [SalesreportComponent],
   imports: [
     CommonModule,
-    SalesreportRoutingModule
+    SalesreportRoutingModule,
+    InputTextModule,
+    DropdownModule,
+    CalendarModule,
+    RadioButtonModule,
+    CheckboxModule,
+    ButtonModule,
+    TableModule,
+    FormsModule
   ]
 })
 export class SalesreportModule { }
--- src/app/salesreport/salesreport/salesreport.component.ts
+++ src/app/salesreport/salesreport/salesreport.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import {SalesreportService}from '../salesreport.service';
 @Component({
   selector: 'app-salesreport',
   templateUrl: './salesreport.component.html',
   styleUrls: ['./salesreport.component.scss']
 })
 export class SalesreportComponent implements OnInit {
-
-  constructor() { }
+frmDate=new Date();
+tdate=new Date();
+selectedDept:string="slctd";
+selectedCategories:string[];
+analysysList:any;
+  constructor(private svc:SalesreportService) {
+this.svc.getSalesAnalysys().pipe().subscribe((value:any)=>{
+this.analysysList=value;
+})
+   }
 
   ngOnInit() {
   }
-
+  onDeptClick(){
+debugger;
+    if(this.selectedDept.toLocaleLowerCase()=="all"){
+      this.selectedCategories=["Nursing Office","Marketing","OPD Treatment Room","Operation Theator","Pharmacy"];
+    }else{
+      this.selectedCategories=[];
+    }
+  }
 }
